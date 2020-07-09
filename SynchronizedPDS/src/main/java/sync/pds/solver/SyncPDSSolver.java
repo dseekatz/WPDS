032509aea86ee78b9f73b60fe1b3eea6377a6a7a
David Seekatz <seekatz@ualberta.ca>
2020-07-09 15:12:24 -0600

    add additional constructor to SPDS

--- SynchronizedPDS/src/main/java/sync/pds/solver/SyncPDSSolver.java
+++ SynchronizedPDS/src/main/java/sync/pds/solver/SyncPDSSolver.java
@@ -181,6 +181,22 @@ public abstract class SyncPDSSolver<
           }
         };
 
+    applyPoststar(callSummaries, fieldSummaries);
+  }
+
+  public SyncPDSSolver(
+      WeightedPAutomaton<Field, INode<Node<Stmt, Fact>>, W> fieldAutomaton,
+      WeightedPAutomaton<Stmt, INode<Fact>, W> callAutomaton,
+      NestedWeightedPAutomatons<Field, INode<Node<Stmt, Fact>>, W> fieldSummaries,
+      NestedWeightedPAutomatons<Stmt, INode<Fact>, W> callSummaries) {
+    this.callAutomaton = callAutomaton;
+    this.fieldAutomaton = fieldAutomaton;
+    applyPoststar(callSummaries, fieldSummaries);
+  }
+
+  private void applyPoststar(
+      NestedWeightedPAutomatons<Stmt, INode<Fact>, W> callSummaries,
+      NestedWeightedPAutomatons<Field, INode<Node<Stmt, Fact>>, W> fieldSummaries) {
     callAutomaton.registerListener(new CallAutomatonListener());
     fieldAutomaton.registerListener(new FieldUpdateListener());
     if (callAutomaton.nested())
